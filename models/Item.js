1e621201b01a93fa1e1d8950fdead95772f48992
ravi17arya01 <ravi.kumar@arya.ai>
2025-06-04 02:37:08 +0530

    Made small changes in schema and readme file

--- models/Item.js
+++ models/Item.js
@@ -4,7 +4,7 @@ const ItemSchema = new mongoose.Schema({
     name: {type: String, required: true},
     quantity: {type: String, required: true},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
-})
+}, { timestamps: true });
 
 const Item = mongoose.model("items", ItemSchema);
 module.exports = Item;
